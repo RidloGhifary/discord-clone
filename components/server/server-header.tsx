351ca7b239f8c0186a103871614a6a7250257758
ridho <ridloghfry@gmail.com>
2024-02-11 06:39:02 +0700

    Channel creation

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -65,7 +65,9 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
           </DropdownMenuItem>
         )}
         {isModerator && (
-          <DropdownMenuItem className="dark:text-neutral-200 px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem
+            className="dark:text-neutral-200 px-3 py-2 text-sm cursor-pointer"
+            onClick={() => onOpen("createChannel")}>
             Create Channel <PlusCircle className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
         )}
