1ecd67f664eb9d48cc0975f8d21df28d12bf7b6f
ridho <ridloghfry@gmail.com>
2024-02-11 08:22:13 +0700

    Delete server

---
+++ components/modals/delete-server-modal.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { useModal } from "@/hooks/use-modal-store";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+export const DeleteServerModal = () => {
+  const { isOpen, onClose, type, data } = useModal();
+  const router = useRouter();
+
+  const isModalOpen = isOpen && type === "deleteServer";
+  const { server } = data;
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onClick = async () => {
+    try {
+      setIsLoading(true);
+      await axios.delete(`/api/servers/${server?.id}`);
+      onClose();
+      router.refresh();
+      router.push("/");
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <Dialog open={isModalOpen} onOpenChange={onClose}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle className="text-center text-2xl font-bold">
+            Delete Server
+          </DialogTitle>
+          <DialogDescription>
+            Are you sure want to do this
+            <br />
+            <span className="font-semibold text-indigo-500">
+              {server?.name} will be permanently deleted.
+            </span>
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter className="px-6 py-4">
+          <div className="flex justify-between items-center w-full">
+            <Button disabled={isLoading} onClick={onClose} variant="outline">
+              Cancel
+            </Button>
+            <Button disabled={isLoading} onClick={onClick} variant="primary">
+              Confirm
+            </Button>
+          </div>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
